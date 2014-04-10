82bc938962d1944c1c7ba4df011dc74034a2ead0
Fred Hsu <fredlhsu@gmail.com>
2014-04-10 15:37:45 -0700

    Added show version

--- eapi.go
+++ eapi.go
@@ -32,16 +32,16 @@ type ShowVersion struct {
     InternalVersion string `json:"internalVersion"`
     SystemMacAddress string `json:"systemMacAddress"`
     SerialNumber string `json:"serialNumber"`
-    MemTotal string `json:"memTotal"`
-    BootupTimestap string `json:"bootupTimestamp"`
-    MemFree string `json:"memFree"`
+    MemTotal float64 `json:"memTotal"`
+    BootupTimestap float64 `json:"bootupTimestamp"`
+    MemFree float64 `json:"memFree"`
     Version string `json:"version"`
     Architecture string `json:"architecture"`
     InternalBuildId string `json:"internalBuildId"`
     HardwareRevision string `json:"hardwareRevision"`
 }
 
-func eapiCall(url string, cmds []string) *http.Response { 
+func eapiCall(url string, cmds []string) JsonRpcResponse {
     p := Parameters{1, cmds, "json"}
     req := Request{"2.0", "runCmds", p, "1"}
     buf, err := json.Marshal(req)
@@ -49,48 +49,48 @@ func eapiCall(url string, cmds []string) *http.Response {
         panic(err)
     }
     resp, err := http.Post(url, "application/json", bytes.NewReader(buf))
+    defer resp.Body.Close()
 
     if err != nil {
         panic(err)
     }
-    return resp
+    return decodeEapiResponse(resp)
 }
 
-func decodeEapiResponse(resp *http.Response) []interface{} {
+func decodeEapiResponse(resp *http.Response) JsonRpcResponse {
     dec := json.NewDecoder(resp.Body)
-
-    // Could I use a different type of variable to decode into
-    // a known struct type?
-    var v map[string]interface{}
+    var v JsonRpcResponse
     if err := dec.Decode(&v); err != nil {
         log.Println(err)
-        return nil
     }
-    return v["result"].([]interface{})
+    return v
 }
 
-func showVersion (resp *http.Response) ShowVersion {
-    dec := json.NewDecoder(resp.Body)
-    // Could I use a different type of variable to decode into
-    // a known struct type?
-    var sv ShowVersion 
-    if err := dec.Decode(&sv); err != nil {
-        log.Println(err)
+func showVersion (jr JsonRpcResponse) ShowVersion {
+    tmp := jr.Result[0].(map[string]interface{})
+    var sv ShowVersion
+    sv = ShowVersion {
+        ModelName: tmp["modelName"].(string),
+        InternalVersion: tmp["internalVersion"].(string),
+        SystemMacAddress: tmp["systemMacAddress"].(string),
+        SerialNumber: tmp["serialNumber"].(string),
+        MemTotal: tmp["memTotal"].(float64),
+        BootupTimestap: tmp["bootupTimestamp"].(float64),
+        MemFree: tmp["memFree"].(float64),
+        Version: tmp["version"].(string),
+        Architecture: tmp["architecture"].(string),
+        InternalBuildId: tmp["internalBuildId"].(string),
+        HardwareRevision: tmp["hardwareRevision"].(string),
     }
-    fmt.Println(sv)
     return sv
 }
 
 func main() {
     cmds := []string{"show version"}
     url := "http://admin:admin@192.168.56.101/command-api/"
-    resp := eapiCall(url, cmds)
-    /*
-    m := decodeEapiResponse(resp)
-    fields := m[0].(map[string]interface{})
-    fmt.Println(fields["version"])
-    */
-    m := showVersion(resp)
-    fmt.Println(m)
+    jr := eapiCall(url, cmds)
+    fmt.Println("result: ", jr.Result)
+    sv := showVersion(jr)
+    fmt.Println("\nVersion: ", sv.Version)
 }
 
