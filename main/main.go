0712bf9bce518027ecd6b555007ca4bc89757416
Fred Hsu <fredlhsu@aristanetworks.com>
2014-06-30 12:13:11 -0700

    Adding use cases for text output

--- main/main.go
+++ main/main.go
@@ -16,27 +16,24 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("\nVersion: ", sv.Version)
-	//configCmds := []string{"enable", "configure", "interface ethernet 1", "descr go"}
-	configCmds := []string{"enable", "configure", "aaa root secret arista"}
-	jr = eapi.Call(url, configCmds, "json")
-	fmt.Println("result: ", jr.Result)
-	fmt.Println("error: ", jr.Error)
-	cmds = []string{"show interfaces"}
-	jr = eapi.Call(url, cmds, "json")
 	var si eapi.ShowInterfaces
-	err = mapstructure.Decode(jr.Result[0], &si)
+	err = mapstructure.Decode(jr.Result[1], &si)
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Println("result: ", si)
 	fmt.Println("result: ", si.Interfaces["Ethernet10"].Description)
 	fmt.Println("result: ", si.Interfaces["Ethernet10"].InterfaceStatistics)
 	fmt.Println("result: ", si.Interfaces["Ethernet10"].Mtu)
 	fmt.Println("result: ", si.Interfaces["Ethernet10"].LineProtocolStatus)
 	fmt.Printf("result: %+v \n", si.Interfaces["Ethernet10"].InterfaceAddress)
 	fmt.Printf("result: %+v \n", si.Interfaces["Ethernet10"].InterfaceCounters.OutErrorsDetail)
-	// for k, v := range si.Interfaces["Ethernet10"].InterfaceCounters.OutErrorsDetail {
-	// 	fmt.Println(k, ":", v)
-	// }
+	//configCmds := []string{"enable", "configure", "interface ethernet 1", "descr go"}
+	//configCmds := []string{"enable", "configure", "aaa root secret arista"}
+	//jr = eapi.Call(url, configCmds, "json")
+	//fmt.Println("result: ", jr.Result)
 
+	cmds = []string{"show ip route", "show ip bgp neighbors"}
+	jr = eapi.Call(url, cmds, "text")
+	fmt.Println(jr.Result[0]["output"])
+	fmt.Println(jr.Result[1]["output"])
 }
